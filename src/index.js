b4cee09470a8d061ddd2fa8c7faeead3ae081347
Mina Halim <mn_halim@yahoo.com>
2023-05-13 01:43:46 -0400

    Adding firebase and fixing the app title

--- src/index.js
+++ src/index.js
@@ -10,6 +10,28 @@ import Playground from "./Pages/Playground";
 
 import "./index.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9Wnodp93-sFlTTpglgsInItLHwH0JGJw",
+  authDomain: "mobius-cf9b2.firebaseapp.com",
+  projectId: "mobius-cf9b2",
+  storageBucket: "mobius-cf9b2.appspot.com",
+  messagingSenderId: "573994344375",
+  appId: "1:573994344375:web:d3e9f24246c95ee4c69232",
+  measurementId: "G-39F5JC7PLM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const router = createBrowserRouter([
   {
     path: "/",
